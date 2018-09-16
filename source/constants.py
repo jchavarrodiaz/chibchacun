483f5260e3b31e5250f5d0b74fad7b0c77059f6c
Andres Velasco <anvelascos@Andress-MacBook-Air.local>
2018-09-15 20:28:50 -0500

    Ajuste a los umbrales de precipitacion

--- source/constants.py
+++ source/constants.py
@@ -1,26 +1,26 @@
 dt_pt_ranges = {
     'Seco': {
         'Lim_Inf': -.1,
-        'Lim_Sup': 2.,
+        'Lim_Sup': .1,
         'Codigos': [1, 2, 3, 991, 992]
     },
     'Lloviznas': {
-        'Lim_Inf': 2.,
-        'Lim_Sup': 5.,
+        'Lim_Inf': .1,
+        'Lim_Sup': 1.,
         'Codigos': [21, 31, 61, 9921, 9961]
     },
     'Lluvias': {
-        'Lim_Inf': 5.,
-        'Lim_Sup': 10.,
+        'Lim_Inf': 1.,
+        'Lim_Sup': 2.,
         'Codigos': [22, 32, 62, 9922, 9962]
     },
     'Moderadas': {
-        'Lim_Inf': 10.,
-        'Lim_Sup': 20.,
+        'Lim_Inf': 2.,
+        'Lim_Sup': 10.,
         'Codigos': [23, 33, 63, 9923]
     },
     'Intensas': {
-        'Lim_Inf': 20.,
+        'Lim_Inf': 10.,
         'Lim_Sup': 1000.,
         'Codigos': [64]
     }
